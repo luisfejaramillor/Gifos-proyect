7563ddc2998b7aebafa72c245556782fc21e975c
luisfejaramillor <luisfejaramillor@gmail.com>
2021-04-07 23:31:04 -0500

    New features added to the proyect, like carousel for the trending section full of gifs pulled from the giphy trending endpoint. Also every single gifs has a hover...waiting to add new functions on the buttos of every gifs

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+////GROUP OF VAR THAT HAVE TO BE DECLARED AS GLOBAL SCOPE FOR SWITCHING TABS PORPUSE/////
+
+
 let tabs = document.querySelectorAll('[data-tab-target]')
 let tabContents = document.querySelectorAll('[data-tab-content]')
 let container = document.querySelector(".container")
@@ -14,59 +17,183 @@ let nightModeDesk = document.querySelector(".night-mode-desk")
 let createButton = document.querySelector(".create-button")
 
 
+/// The code below is use to switch between tabs in the mobile and desktop version
+
 tabs.forEach(tab => {
     tab.addEventListener("click", () => {
         let target = document.querySelectorAll(tab.dataset.tabTarget)
         tabContents.forEach(tabContent => {
             tabContent.classList.add("inactive")
         })
-        target.forEach(content =>{
+        target.forEach(content => {
             content.classList.remove("inactive")
         })
-        if(burguerButton.src==="http://127.0.0.1:5500/resources/burger.svg"){
+        if (burguerButton.src === "http://127.0.0.1:5500/resources/burger.svg") {
             burguerButton.src = "http://127.0.0.1:5500/resources/close.svg"
-        }
-        else{
+        } else {
             burguerButton.src = "http://127.0.0.1:5500/resources/burger.svg"
-            dropMenu.classList.add("inactive")   
+            dropMenu.classList.add("inactive")
             trendingSection.classList.remove("inactive")
             footer.classList.remove("inactive")
         }
     })
 })
 
-logo.addEventListener("click", ()=>{
+logo.addEventListener("click", () => {
     burguerButton.src = "http://127.0.0.1:5500/resources/burger.svg"
 })
 
 /////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////
-nightMode.addEventListener("click", ()=>{
-    if(nightMode.innerHTML==="Modo Nocturno"){
-        nightMode.innerHTML="Modo Diurno"
+///    The code below is use to switch between light and dark mode ////
+nightMode.addEventListener("click", () => {
+    if (nightMode.innerHTML === "Modo Nocturno") {
+        nightMode.innerHTML = "Modo Diurno"
         container.classList.toggle("dark")
-    }else{
-        nightMode.innerHTML="Modo Nocturno"
-        container.classList.toggle("dark")
-    }
-
-})
-
-nightModeDesk.addEventListener("click", ()=>{
-    if(nightModeDesk.innerHTML==="MODO NOCTURNO"){
-        nightModeDesk.innerHTML="MODO DIURNO"
-        container.classList.toggle("dark")
-    }else{
-        nightModeDesk.innerHTML="MODO NOCTURNO"
+    } else {
+        nightMode.innerHTML = "Modo Nocturno"
         container.classList.toggle("dark")
     }
 
 })
 
-createButton.addEventListener("click", ()=>{
+nightModeDesk.addEventListener("click", () => {
+    if (nightModeDesk.innerHTML === "MODO NOCTURNO") {
+        nightModeDesk.innerHTML = "MODO DIURNO"
+        container.classList.toggle("dark")
+    } else {
+        nightModeDesk.innerHTML = "MODO NOCTURNO"
+        container.classList.toggle("dark")
+    }
+
+})
+
+createButton.addEventListener("click", () => {
     trendingSection.classList.add("inactive")
 })
 
 
+/////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+//// The code below is use to operate the carousel of the trending section ////
+
+let gifKey = "Kgtc3PtS5s0QmceWRa7YZEb3CAR82e95"
+let containerCarousel = document.querySelector(".gifs-trending-wrapper")
+let buttonSlider = document.querySelectorAll(".button-slider")
+let numberOfGifs = 30
+let imageIndex = 1
+let translateX = 0
+buttonSlider.forEach(button => {
+    button.addEventListener("click", (e) => {
+        if (e.target.className === "button-slider left") {
+            if (imageIndex !== 1) {
+                imageIndex--
+                translateX += 280
+            }
+        } else if (imageIndex !== numberOfGifs) {
+            imageIndex++
+            translateX -= 280
+        }
+        containerCarousel.style.transform = `translateX(${translateX}px)`
+    })
+})
+
+/*
+function displayWindowSize() {
+    var width = document.documentElement.clientWidth;
+    console.log(width)
+    if (width < 700) {
+        numberOfGifs = 3
+        return 250
+    }
+}
+window.addEventListener("resize", displayWindowSize);
+displayWindowSize();
+*/
 
 
+
+//////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+///// The code below is use to pull gifs from giphy trendings endpoint ////
+
+
+let getTrendingGifs =  async (number)=>{
+    let url = `https://api.giphy.com/v1/gifs/trending?api_key=${gifKey}&limit=${number}`
+    let response = await fetch(url)
+    let json = await response.json()
+    let data = json.data
+    return data
+}
+
+let getUrlsAndDrawGifs = ()=>{
+    let data = getTrendingGifs(30)
+    data.then(res =>{
+        res.forEach(gif =>{
+            let url = gif.images.original.url
+            let title = gif.title
+            createDivTrending(url, gif.username, title) 
+        })
+    }).catch((err)=>{
+        console.log(err)
+    })
+}
+
+let createDivTrending = (url, user, title)=>{
+    let div = document.createElement("div")
+    div.classList.add("gifs-trending")
+    containerCarousel.appendChild(div)
+    createImgTrending(div, url, user, title)
+}
+
+let createImgTrending = (div, url, user, title)=>{
+    let img = document.createElement("img")
+    img.setAttribute("src", url)
+    div.appendChild(img)
+    createDivOverlay(div, user, title)
+}
+
+let createDivOverlay = (div, user, title)=>{
+    let divOverlay = document.createElement("div")
+    divOverlay.classList.add("overlay")
+    div.appendChild(divOverlay)
+    createDivsChildrenOverlay(divOverlay, user, title)
+}
+
+let createDivsChildrenOverlay = (divFather, user, title)=>{
+    let divChildren1 = document.createElement("div")
+    divChildren1.classList.add("children1")
+    let divChildren2 = document.createElement("div")
+    divChildren2.classList.add("children2")
+    divFather.appendChild(divChildren1)
+    divFather.appendChild(divChildren2)
+    createUserTitle(divChildren1, user, title)
+    createButtonGifs(divChildren2)
+}
+
+let createUserTitle = (childre1, user, title)=>{
+    let username = document.createElement("h3")
+    username.innerHTML=user
+    let titleGif = document.createElement("h2")
+    titleGif.innerHTML=title
+    childre1.appendChild(username)
+    childre1.appendChild(titleGif)
+}
+
+let createButtonGifs = (children2)=>{
+    let favorite = document.createElement("img")
+    favorite.setAttribute("src", "http://127.0.0.1:5500/resources/icon-fav.svg")
+    let download = document.createElement("img")
+    download.setAttribute("src", "http://127.0.0.1:5500/resources/icon-download.svg")
+    let expand = document.createElement("img")
+    expand.setAttribute("src", "http://127.0.0.1:5500/resources/icon-max-hover.svg")
+    children2.appendChild(favorite)
+    children2.appendChild(download)
+    children2.appendChild(expand)
+}
+
+getUrlsAndDrawGifs()
+
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
