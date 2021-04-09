40734a235573272563ffbd408881b0264558d347
luisfejaramillor <luisfejaramillor@gmail.com>
2021-04-08 23:52:34 -0500

    Expand button totally fuctional..just need to shape cards button properly  04/08/2021

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 ////GROUP OF VAR THAT HAVE TO BE DECLARED AS GLOBAL SCOPE FOR SWITCHING TABS PORPUSE/////
-
-
-let tabs = document.querySelectorAll('[data-tab-target]')
 let tabContents = document.querySelectorAll('[data-tab-content]')
 let container = document.querySelector(".container")
 let searchResults = document.querySelector(".search-results")
@@ -16,32 +13,39 @@ let nightMode = document.querySelector(".night-mode")
 let nightModeDesk = document.querySelector(".night-mode-desk")
 let createButton = document.querySelector(".create-button")
 
-
 /// The code below is use to switch between tabs in the mobile and desktop version
-
-tabs.forEach(tab => {
-    tab.addEventListener("click", () => {
-        let target = document.querySelectorAll(tab.dataset.tabTarget)
-        tabContents.forEach(tabContent => {
-            tabContent.classList.add("inactive")
+let getTabs = () => {
+    setTimeout(() => {
+        let tabs = document.querySelectorAll('[data-tab-target]')
+        callSwitcher(tabs)
+    }, 1000);
+}
+getTabs()
+let callSwitcher = (tabs) => {
+    tabs.forEach(tab => {
+        tab.addEventListener("click", () => {
+            let target = document.querySelectorAll(tab.dataset.tabTarget)
+            tabContents.forEach(tabContent => {
+                tabContent.classList.add("inactive")
+            })
+            target.forEach(content => {
+                content.classList.remove("inactive")
+            })
+            if (burguerButton.src === "http://127.0.0.1:5500/resources/burger.svg") {
+                burguerButton.src = "http://127.0.0.1:5500/resources/close.svg"
+            } else {
+                burguerButton.src = "http://127.0.0.1:5500/resources/burger.svg"
+                dropMenu.classList.add("inactive")
+                trendingSection.classList.remove("inactive")
+                footer.classList.remove("inactive")
+            }
         })
-        target.forEach(content => {
-            content.classList.remove("inactive")
-        })
-        if (burguerButton.src === "http://127.0.0.1:5500/resources/burger.svg") {
-            burguerButton.src = "http://127.0.0.1:5500/resources/close.svg"
-        } else {
+        logo.addEventListener("click", () => {
             burguerButton.src = "http://127.0.0.1:5500/resources/burger.svg"
-            dropMenu.classList.add("inactive")
-            trendingSection.classList.remove("inactive")
-            footer.classList.remove("inactive")
-        }
+        })
     })
-})
+}
 
-logo.addEventListener("click", () => {
-    burguerButton.src = "http://127.0.0.1:5500/resources/burger.svg"
-})
 
 /////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////
@@ -89,11 +93,11 @@ buttonSlider.forEach(button => {
         if (e.target.className === "button-slider left") {
             if (imageIndex !== 1) {
                 imageIndex--
-                translateX += 280
+                translateX += 200
             }
         } else if (imageIndex !== numberOfGifs) {
             imageIndex++
-            translateX -= 280
+            translateX -= 200
         }
         containerCarousel.style.transform = `translateX(${translateX}px)`
     })
@@ -119,7 +123,7 @@ displayWindowSize();
 ///// The code below is use to pull gifs from giphy trendings endpoint ////
 
 
-let getTrendingGifs =  async (number)=>{
+let getTrendingGifs = async (number) => {
     let url = `https://api.giphy.com/v1/gifs/trending?api_key=${gifKey}&limit=${number}`
     let response = await fetch(url)
     let json = await response.json()
@@ -127,41 +131,41 @@ let getTrendingGifs =  async (number)=>{
     return data
 }
 
-let getUrlsAndDrawGifs = ()=>{
+let getUrlsAndDrawGifs = () => {
     let data = getTrendingGifs(30)
-    data.then(res =>{
-        res.forEach(gif =>{
+    data.then(res => {
+        res.forEach(gif => {
             let url = gif.images.original.url
             let title = gif.title
-            createDivTrending(url, gif.username, title) 
+            createDivTrending(url, gif.username, title)
         })
-    }).catch((err)=>{
+    }).catch((err) => {
         console.log(err)
     })
 }
 
-let createDivTrending = (url, user, title)=>{
+let createDivTrending = (url, user, title) => {
     let div = document.createElement("div")
     div.classList.add("gifs-trending")
     containerCarousel.appendChild(div)
     createImgTrending(div, url, user, title)
 }
 
-let createImgTrending = (div, url, user, title)=>{
+let createImgTrending = (div, url, user, title) => {
     let img = document.createElement("img")
     img.setAttribute("src", url)
     div.appendChild(img)
     createDivOverlay(div, user, title)
 }
 
-let createDivOverlay = (div, user, title)=>{
+let createDivOverlay = (div, user, title) => {
     let divOverlay = document.createElement("div")
     divOverlay.classList.add("overlay")
     div.appendChild(divOverlay)
     createDivsChildrenOverlay(divOverlay, user, title)
 }
 
-let createDivsChildrenOverlay = (divFather, user, title)=>{
+let createDivsChildrenOverlay = (divFather, user, title) => {
     let divChildren1 = document.createElement("div")
     divChildren1.classList.add("children1")
     let divChildren2 = document.createElement("div")
@@ -172,22 +176,29 @@ let createDivsChildrenOverlay = (divFather, user, title)=>{
     createButtonGifs(divChildren2)
 }
 
-let createUserTitle = (childre1, user, title)=>{
+let createUserTitle = (childre1, user, title) => {
     let username = document.createElement("h3")
-    username.innerHTML=user
+    username.innerHTML = user
     let titleGif = document.createElement("h2")
-    titleGif.innerHTML=title
+    titleGif.innerHTML = title
     childre1.appendChild(username)
     childre1.appendChild(titleGif)
 }
 
-let createButtonGifs = (children2)=>{
+let createButtonGifs = (children2) => {
     let favorite = document.createElement("img")
     favorite.setAttribute("src", "http://127.0.0.1:5500/resources/icon-fav.svg")
+    favorite.setAttribute("class", "add-favorite")
+
     let download = document.createElement("img")
     download.setAttribute("src", "http://127.0.0.1:5500/resources/icon-download.svg")
+    download.setAttribute("class", "download")
+
     let expand = document.createElement("img")
     expand.setAttribute("src", "http://127.0.0.1:5500/resources/icon-max-hover.svg")
+    expand.setAttribute("class", "expandButton")
+    expand.setAttribute("data-tab-target", ".refExpand")
+
     children2.appendChild(favorite)
     children2.appendChild(download)
     children2.appendChild(expand)
@@ -197,3 +208,16 @@ getUrlsAndDrawGifs()
 
 ////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
+
+let expandContainer = document.querySelector(".expand-gif")
+setTimeout(() => {
+    let expandButton = document.querySelectorAll(".expandButton")
+    expandButton.forEach(button => {
+        button.addEventListener("click", (e) => {
+            expandContainer.innerHTML = ""
+            let cardDiv = e.path[3]
+            let clone = cardDiv.cloneNode(true)
+            expandContainer.appendChild(clone)
+        })
+    })
+}, 1000);
