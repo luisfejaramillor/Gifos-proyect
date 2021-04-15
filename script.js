e52576de5a1949a202fd7ea5f362d38b865e6667
luisfejaramillor <luisfejaramillor@gmail.com>
2021-04-14 23:33:12 -0500

    Search result and suggestions function is now totally operative

--- script.js
+++ script.js
@@ -21,13 +21,18 @@ let getTabs = () => {
     }, 1000);
 }
 getTabs()
+
+
 let callSwitcher = (tabs) => {
+
     tabs.forEach(tab => {
         tab.addEventListener("click", () => {
             let target = document.querySelectorAll(tab.dataset.tabTarget)
+
             tabContents.forEach(tabContent => {
                 tabContent.classList.add("inactive")
             })
+
             target.forEach(content => {
                 content.classList.remove("inactive")
             })
@@ -213,9 +218,12 @@ let expandContainer = document.querySelector(".expand-gif")
 
 setTimeout(() => {
     let expandButton = document.querySelectorAll(".expandButton")
+    expandCards(expandButton)
+}, 1000);
+
+let expandCards = (expandButton) => {
     expandButton.forEach(button => {
         button.addEventListener("click", (e) => {
-            console.log(e.path[3].childNodes)
             expandContainer.innerHTML = ""
             let cardDivGif = e.path[3].childNodes[0]
             let cardDivOverlay = e.path[3].childNodes[1]
@@ -223,9 +231,30 @@ setTimeout(() => {
             let cloneOverlay = cardDivOverlay.cloneNode(true)
             expandContainer.appendChild(cloneGif)
             expandContainer.appendChild(cloneOverlay)
+            hideELements()
+            if (burguerButton.src === "http://127.0.0.1:5500/resources/burger.svg") {
+                burguerButton.src = "http://127.0.0.1:5500/resources/close.svg"
+            }
         })
     })
-}, 1000);
+}
+burguerButton.addEventListener("click", () => {
+    let main = document.querySelector(".main")
+    if (burguerButton.src === "http://127.0.0.1:5500/resources/close.svg") {
+        expandContainer.classList.add("inactive")
+        main.classList.remove("inactive")
+    }
+})
+
+let hideELements = () => {
+    let main = document.querySelector(".main")
+    if (expandContainer.innerHTML !== "") {
+        expandContainer.classList.remove("inactive")
+        main.classList.add("inactive")
+        console.log(burguerButton)
+    }
+}
+
 
 
 
@@ -238,15 +267,23 @@ let searchButton = document.querySelector(".icon-search")
 let searchButtonHidden = document.querySelector(".icon-search-hidden")
 let suggestionList = document.querySelector(".suggestion-list")
 let shapper = document.querySelector(".shapper")
-inputSearch.addEventListener("keyup",()=>{
 
-    if(inputSearch.value !== ""){
+inputSearch.addEventListener("keyup", (e) => {
+    if (inputSearch.value !== "") {
+        if(e.keyCode === 13){
+            changeSeeMoreContainer(inputSearch.value)
+        }
+        searchButtonHidden.addEventListener("click",()=>{
+            changeSeeMoreContainer(inputSearch.value)
+        })
+        getValueSuggestions()
         suggestionList.innerHTML = ""
         searchButton.src = "/resources/close-input.svg"
         searchButtonHidden.classList.remove("inactive")
         divInputContainer.style.height = `182.97px`
+
         searchSuggestions(inputSearch.value)
-    }else{
+    } else {
         divInputContainer.style.height = ``
         searchButton.src = "/resources/icon-search.svg"
         searchButtonHidden.classList.add("inactive")
@@ -254,8 +291,8 @@ inputSearch.addEventListener("keyup",()=>{
     }
 })
 
-divInputContainer.addEventListener("click",(e)=>{
-    if(e.target.src === "http://127.0.0.1:5500/resources/close-input.svg"){
+divInputContainer.addEventListener("click", (e) => {
+    if (e.target.src === "http://127.0.0.1:5500/resources/close-input.svg") {
         divInputContainer.style.height = ``
         searchButton.src = "/resources/icon-search.svg"
         searchButtonHidden.classList.add("inactive")
@@ -264,30 +301,158 @@ divInputContainer.addEventListener("click",(e)=>{
     }
 })
 
+let getValueSuggestions = () => {
+    setTimeout(() => {
+        suggestionList.childNodes.forEach(child => {
+            let suggesValue = child.childNodes[1].innerHTML
+            child.childNodes.forEach(subChild => {
+                subChild.addEventListener("click", () => {
+                    changeSeeMoreContainer(suggesValue)
+                })
+            })
+        })
+    }, 200);
+}
+
+let changeSeeMoreContainer = (suggesValue)=>{
+    gifsSearchContainer.style.height = `${1425}px`
+    indexSeeMore = 1
+    heightSeeMore = 1425
+    gifsSearchContainer.innerHTML = ""
+    searchResultsGifs(suggesValue, 30)
+    buttonSeeMore.classList.remove("inactive")
+    setTimeout(() => {
+        let expandButton = document.querySelectorAll(".expandButton")
+        expandCards(expandButton)
+    }, 300);
+}
 
 ////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////
+let gifsSearchContainer = document.querySelector(".gifs-search-container")
 
-let searchSuggestions = async (input)=>{
+
+let searchSuggestions = async (input) => {
     let url = `https://api.giphy.com/v1/gifs/search/tags?api_key=${gifKey}&q=${input}`
     let result = await fetch(url)
     let json = await result.json()
-    json.data.forEach(sugges =>{
+    json.data.forEach(sugges => {
         createDivSuggestion(sugges.name)
     })
 }
 
-let createDivSuggestion = (name)=>{
+let createDivSuggestion = (name) => {
     let div = document.createElement("div")
     createLiImgSuggestion(div, name)
     suggestionList.appendChild(div)
 }
 
-let createLiImgSuggestion = (div, name)=>{
+let createLiImgSuggestion = (div, name) => {
     let img = document.createElement("img")
     let li = document.createElement("li")
     li.innerHTML = name
     img.setAttribute("src", "http://127.0.0.1:5500/resources/icon-search.svg")
     div.appendChild(img)
     div.appendChild(li)
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+
+let searchResultsGifs = async (input, number) => {
+    let url = `https://api.giphy.com/v1/gifs/search?api_key=${gifKey}&q=${input}&limit=${number}`
+    let result = await fetch(url)
+    let json = await result.json()
+    json.data.forEach(gif => {
+        let url = gif.images.original.url
+        let title = gif.title
+        let username = gif.username
+        createDivSearchGif(url, title, username)
+    })
+}
+
+
+
+let createDivSearchGif = (url, title, username) => {
+    let div = document.createElement("div")
+    div.classList.add("gifs-searching")
+    gifsSearchContainer.appendChild(div)
+    createImgSearchGif(div, url, title, username)
+}
+let createImgSearchGif = (div, url, title, username) => {
+    let img = document.createElement("img")
+    img.setAttribute("src", url)
+    div.appendChild(img)
+    createDivOverlaySearch(div, title, username)
+}
+
+let createDivOverlaySearch = (div, title, username) => {
+    let divOverlay = document.createElement("div")
+    divOverlay.classList.add("overlay")
+    div.appendChild(divOverlay)
+    createDivsChildrenOverlaySearch(divOverlay, title, username)
+}
+
+let createDivsChildrenOverlaySearch = (divFather, title, username) => {
+    let divChildren1 = document.createElement("div")
+    divChildren1.classList.add("children1")
+    let divChildren2 = document.createElement("div")
+    divChildren2.classList.add("children2")
+
+    divFather.appendChild(divChildren1)
+    divFather.appendChild(divChildren2)
+    createUserTitleSearch(divChildren1, username, title)
+    createButtonGifsSearch(divChildren2)
+}
+let createUserTitleSearch = (children1, user, title) => {
+    let username = document.createElement("h3")
+    username.innerHTML = user
+    let titleGif = document.createElement("h2")
+    titleGif.innerHTML = title
+    children1.appendChild(username)
+    children1.appendChild(titleGif)
+}
+let createButtonGifsSearch = (children2) => {
+    let favorite = document.createElement("img")
+    favorite.setAttribute("src", "http://127.0.0.1:5500/resources/icon-fav.svg")
+    favorite.setAttribute("class", "add-favorite")
+
+    let download = document.createElement("img")
+    download.setAttribute("src", "http://127.0.0.1:5500/resources/icon-download.svg")
+    download.setAttribute("class", "download")
+
+    let expand = document.createElement("img")
+    expand.setAttribute("src", "http://127.0.0.1:5500/resources/icon-max-hover.svg")
+    expand.setAttribute("class", "expandButton")
+    expand.setAttribute("data-tab-target", ".refExpand")
+
+    children2.appendChild(favorite)
+    children2.appendChild(download)
+    children2.appendChild(expand)
+}
+
+/////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+let buttonSeeMore = document.querySelector(".see-more")
+let indexSeeMore = 1
+let heightSeeMore = 1425
+
+buttonSeeMore.addEventListener("click", () => {
+    changeHeight()
+})
+
+let changeHeight = () => {
+    indexSeeMore++
+    heightSeeMore += 1445
+    if(indexSeeMore === 5){
+        gifsSearchContainer.style.height = `${heightSeeMore}px`
+        setTimeout(() => {
+            
+            buttonSeeMore.classList.add("inactive")
+            
+         }, 200);
+    }
+    else{
+        gifsSearchContainer.style.height = `${heightSeeMore}px`
+    }
 }
